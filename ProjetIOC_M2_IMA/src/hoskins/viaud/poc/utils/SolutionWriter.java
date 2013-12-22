80a2c3a17e28ec833cb67a1bd47b8e641942ac38
Quentin <Quentin@here>
2013-12-22 10:55:23 +0100

    Solution writer : done

--- ProjetIOC_M2_IMA/src/hoskins/viaud/poc/utils/SolutionWriter.java
+++ ProjetIOC_M2_IMA/src/hoskins/viaud/poc/utils/SolutionWriter.java
@@ -3,11 +3,125 @@
  */
 package hoskins.viaud.poc.utils;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import hoskins.viaud.poc.structure.Instance;
+import hoskins.viaud.poc.structure.Solution;
+
 /**
  * Write a solution in a .csv file
  * @author Maxim HOSKINS and Quentin VIAUD
  *
  */
 public class SolutionWriter {
+
+	/**
+	 * Solution created by solving method
+	 */
+	private Solution s;
+
+	/**
+	 * File name of the source file
+	 */
+	private String sourceFileName;
+
+	/**
+	 * Folder path to store results
+	 */
+	private String folderPath;
+
+	/**
+	 * FileWriter
+	 */
+	private FileWriter fw;
+
+	/**
+	 * PrintWriter
+	 */
+	private PrintWriter out;
+
+	/**
+	 * Create result file
+	 * @param s solution
+	 * @param sourceFileName instance file name
+	 * @param folderPath folder to store result file
+	 * @throws Exception
+	 */
+	public SolutionWriter(Solution s, String sourceFileName, String folderPath) throws Exception{
+		if (!new File(folderPath).exists()) {
+			throw new Exception("Destination folder does not exist");
+		}
+		this.s = s;
+		this.sourceFileName = sourceFileName;
+		this.folderPath = folderPath;
+		openFile(createFileName());
+		buildResult();
+		closeFile();
+	}
+
+	/**
+	 * Build file
+	 */
+	private void buildResult(){
+		out.println(this.s.getOf());
+
+		//Write overtime for all teams
+		for(int i = 0; i < Instance.instance.getNe(); i++)
+			if(i < Instance.instance.getNe() - 1)
+				out.print(this.s.getH()[i] + ";");
+			else
+				out.println(this.s.getH()[i]);
+
+		//Write assignment between team and operation
+		for(int i = 0; i < Instance.instance.getNe(); i++){
+			for(int j = 0; j < Instance.instance.getNo(); j++){
+				if(j < Instance.instance.getNo() - 1)
+					out.print(this.s.getX()[i][j] + ";");
+				else
+					out.print(this.s.getX()[i][j]);
+			}
+			if(i < Instance.instance.getNe() - 1)
+				out.print("\n");
+		}
+	}
+
+	/**
+	 * Open file to write in
+	 * @param filename name and path of file
+	 */
+	private void openFile(String filename){
+		try {
+			this.fw = new FileWriter(filename);
+			this.out = new PrintWriter(fw);
+		} catch (IOException e) {
+			System.out.println("Unable to open file to write");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Close file and print writers
+	 */
+	private void closeFile(){	
+		try {
+			this.out.flush();
+			this.out.close();
+			this.fw.close();
+		} catch (IOException e) {
+			System.out.println("Unable to close file");
+			e.printStackTrace();
+		}  
+	}
+
+	/**
+	 * Create a filename based on source file name
+	 * @return file name and path
+	 */
+	private String createFileName(){
+		return this.folderPath + "/R" + this.sourceFileName;
+	}
 
 }
--- ProjetIOC_M2_IMA/src/sandbox/Main.java
+++ ProjetIOC_M2_IMA/src/sandbox/Main.java
@@ -7,9 +7,9 @@ import hoskins.viaud.poc.heuristic.Heuristic;
 import hoskins.viaud.poc.metaheuristic.VNS;
 import hoskins.viaud.poc.structure.Solution;
 import hoskins.viaud.poc.utils.InstanceReader;
+import hoskins.viaud.poc.utils.SolutionWriter;
 
 import java.io.File;
-import java.io.IOException;
 
 /**
  * @author Maxim HOSKINS and Quentin VIAUD
@@ -19,15 +19,15 @@ public class Main {
 
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		//Folder where instance files are located and parameters of the problem in hour
-		String folderPath = "./instances";
+		String instanceFolderPath = "./instances"; String resultFolderPath = "./results";
 		int l = 12; int s = 3; int c = 60;
 		
 		//Get all files to read
-		File[] listOfFiles = new File(folderPath).listFiles();
+		File[] listOfFiles = new File(instanceFolderPath).listFiles();
 		
 		//Count number of fails
 		int counter = 0;
@@ -49,6 +49,7 @@ public class Main {
 				counter++;
 			
 			//Write the solution in a .csv file
+			new SolutionWriter(sol, file.getName(), resultFolderPath);
 			
 		}
 		
