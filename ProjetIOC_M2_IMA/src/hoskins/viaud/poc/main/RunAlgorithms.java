219c10b58733a319afcf5b1495b5e86c8c736187
maxhoskins <maxim.hoskins@gmail.com>
2014-01-23 14:13:43 +0100

    Started coding main but Quentin was bored so passed on the task ;)

---
+++ ProjetIOC_M2_IMA/src/hoskins/viaud/poc/main/RunAlgorithms.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package hoskins.viaud.poc.main;
+
+/**
+ * Main class that runs all algorithms and writes the solutions to file
+ * @author Maxim HOSKINS and Quentin VIAUD
+ *
+ */
+public class RunAlgorithms {
+	
+	private final String resultCG = "./resultsCG/";
+	private final String resultH = "./resultsH/";
+	private final String resultLagrange = "./resultsLagrange/";
+	private final String resultMSEA = "./resultsMSEA/";
+	private final String resultVNS = "./resultsVNS/";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
